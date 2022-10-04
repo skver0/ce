16b9f9c445c85fc3e3ceada61cd6b6c940120319
mrdeadi <42602656+mrdeadi02@users.noreply.github.com>
2022-10-04 16:28:24 +0200

    defineok rendszerezve, masodik módosítva a megmeg define miatt

--- példa/masodik-legkisebb.c
+++ példa/masodik-legkisebb.c
@@ -7,12 +7,12 @@ egész kezdő nyitózárójel zárózárójel kódblokkeleje
 	egész min értékadás tömb szögletesnyitózárójel 0 szögleteszárózárójel sorvége
 	egész min2 értékadás tömb szögletesnyitózárójel 0 szögleteszárózárójel sorvége
 
-	ciklus nyitózárójel egész i értékadás 0 sorvége i kisebbmint mérete nyitózárójel tömb zárózárójel osztás mérete nyitózárójel tömb szögletesnyitózárójel 0 szögleteszárózárójel zárózárójel sorvége i++ zárózárójel kódblokkeleje
+	ciklus nyitózárójel egész i értékadás 0 sorvége i kisebbmint mérete nyitózárójel tömb zárózárójel osztás mérete nyitózárójel tömb szögletesnyitózárójel 0 szögleteszárózárójel zárózárójel sorvége i megmeg zárózárójel kódblokkeleje
 		ha nyitózárójel min nagyobbmint tömb szögletesnyitózárójel i szögleteszárózárójel zárózárójel
 			min értékadás tömb szögletesnyitózárójel i szögleteszárózárójel sorvége
 	kódblokkvége
 
-	ciklus nyitózárójel egész i értékadás 0 sorvége i kisebbmint mérete nyitózárójel tömb zárózárójel osztás mérete nyitózárójel tömb szögletesnyitózárójel 0 szögleteszárózárójel zárózárójel sorvége i++ zárózárójel kódblokkeleje
+	ciklus nyitózárójel egész i értékadás 0 sorvége i kisebbmint mérete nyitózárójel tömb zárózárójel osztás mérete nyitózárójel tömb szögletesnyitózárójel 0 szögleteszárózárójel zárózárójel sorvége i megmeg zárózárójel kódblokkeleje
 		ha nyitózárójel min2 nagyobbmint tömb szögletesnyitózárójel i szögleteszárózárójel és min nemegyenlő tömb szögletesnyitózárójel i szögleteszárózárójel zárózárójel
 			min2 értékadás tömb szögletesnyitózárójel i szögleteszárózárójel sorvége
 	kódblokkvége
