fc0c3e6ae89ba025f8d3e113b0dcf45b98f9521d
Sárdi Károly <skverbox@gmail.com>
2022-10-20 20:14:13 +0200

    A híres nyolc királnyő, cé-ben

---
+++ példa/nyolc-királynő.c
@@ -0,0 +1,63 @@
+#include "../cé.h"
+#include <stdio.h>
+
+cékód(
+egész oszlopok szögletesnyitózárójel 8 szögleteszárózárójel sorvége
+
+egész ellenőr nyitózárójel egész sor vessző egész oszlop zárózárójel
+kódblokkeleje
+	ciklus nyitózárójel egész i értékadás 0 sorvége i kisebbmint 8 sorvége i megmeg zárózárójel 
+  kódblokkeleje
+		ha nyitózárójel oszlopok szögletesnyitózárójel i szögleteszárózárójel egyenlő sor zárózárójel 
+			visszatér 1 sorvége
+	kódblokkvége
+
+	ciklus nyitózárójel egész i értékadás 0 sorvége i kisebbvagyegyenlő oszlop és i kisebbvagyegyenlő sor sorvége i megmeg zárózárójel
+  kódblokkeleje
+		ha nyitózárójel oszlopok szögletesnyitózárójel oszlop kivonás i szögleteszárózárójel egyenlő sor kivonás i zárózárójel
+			visszatér 1 sorvége
+	kódblokkvége
+
+	ciklus nyitózárójel egész i értékadás 0 sorvége i kisebbvagyegyenlő oszlop és sor összeadás i kisebbmint 8 sorvége i megmeg zárózárójel
+  kódblokkeleje
+		ha nyitózárójel oszlopok szögletesnyitózárójel oszlop kivonás i szögleteszárózárójel egyenlő sor összeadás i zárózárójel
+			visszatér 1 sorvége
+	kódblokkvége
+
+	visszatér 0 sorvége
+kódblokkvége
+
+egész számolás nyitózárójel egész oszlop zárózárójel
+kódblokkeleje
+  ha nyitózárójel oszlop nagyobbvagyegyenlő 8 zárózárójel
+    visszatér 1 sorvége
+
+  egész r értékadás 0 sorvége
+
+  ciklus nyitózárójel egész i értékadás 0 sorvége i kisebbmint 8 sorvége i megmeg zárózárójel
+  kódblokkeleje
+    ha nyitózárójel ellenőr nyitózárójel i vessző oszlop zárózárójel egyenlő 0 zárózárójel
+    kódblokkeleje
+      oszlopok szögletesnyitózárójel oszlop szögleteszárózárójel értékadás i sorvége
+      r += számolás nyitózárójel oszlop összeadás 1 zárózárójel sorvége
+      oszlopok szögletesnyitózárójel oszlop szögleteszárózárójel értékadás -1 sorvége
+    kódblokkvége
+  kódblokkvége
+  visszatér r sorvége
+kódblokkvége
+
+
+egész kezdő nyitózárójel zárózárójel
+kódblokkeleje
+  ciklus nyitózárójel egész i értékadás 0 sorvége i kisebbmint 8 sorvége i megmeg zárózárójel
+  kódblokkeleje
+    oszlopok szögletesnyitózárójel i szögleteszárózárójel értékadás -1 sorvége
+  kódblokkvége
+  
+  egész szumma értékadás számolás nyitózárójel 0 zárózárójel sorvége
+
+  kiír nyitózárójel nyitóidézőjel %d\n záróidézőjel vessző szumma zárózárójel sorvége
+
+  visszatér 0 sorvége
+kódblokkvége
+)
